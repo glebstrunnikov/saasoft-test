000074b6d52172c6387c2689ad41a9be5c0b303d
Gleb Strunnikov <glebstrunnikov@gmail.com>
2024-12-12 01:16:55 +0200

    initial commit with title

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div class="main-container container">
+		<div class="page-title">
+			<h1>Учетные записи</h1>
+			<div class="add-btn">
+				<button type="button" class="btn btn-outline-dark">
+					<img
+						class="btn-icon"
+						src="/src/assets/svg/plus.svg"
+						alt=""
+					/>
+				</button>
+			</div>
+		</div>
+		<div class="page-footnote">
+			<img
+				src="/src/assets/svg/question.svg"
+				alt=""
+				class="footnote-icon"
+			/><span class="footnote-text"
+				>Для указания нескольких меток для одной пары логин/пароль
+				используйте разделитель ;</span
+			>
+		</div>
+	</div>
+</template>
+
+<style lang="sass" scoped>
+.btn-icon
+  width: 30px
+  height: 30px
+.page-title
+  display: flex
+  align-items: center
+.page-footnote
+  background-color: #eff4fa
+</style>
